50a5240b967dc8120b9eebe1658626df96273cf7
SKRIPPII <93274805+SKRIPPII@users.noreply.github.com>
2021-10-27 19:39:09 +0300

    Create Program.cs
    Test

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace procect {
+        class Program{
+            static void Main(){
+                Console.WriteLine("Hello world!\nlox"); 
+                Console.ReadKey();  
+                Console.ReadLine(); 
+                            }
+                      }
+                    }
